9c72338449e7b110cff78c566e4628db1f98c28a
Snowy <34697265+snooooowy@users.noreply.github.com>
2023-05-25 00:32:28 +0800

    spacing

--- EurekaHelper/System/ZoneManager.cs
+++ EurekaHelper/System/ZoneManager.cs
@@ -41,7 +41,7 @@ namespace EurekaHelper.System
             }
         }
 
-        [Signature("E8 ?? ?? ?? ?? 45 33 C0 48 8D 53 10 8B CE E8 ?? ?? ?? ?? 48 8D 4B 64 ", DetourName = nameof(InitZoneDetour))]
+        [Signature("E8 ?? ?? ?? ?? 45 33 C0 48 8D 53 10 8B CE E8 ?? ?? ?? ?? 48 8D 4B 64", DetourName = nameof(InitZoneDetour))]
         private readonly Hook<InitZoneDelegate> InitZoneHook = null!;
 
         private nint InitZoneDetour(nint a1, int a2, nint a3)
